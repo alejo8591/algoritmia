644c247faa732b06e58dac720e7d27646d8d0584
Alejandro Romero <alejo8591@gmail.com>
2012-03-27 21:06:14 -0500

    Actualizando algoritmo de busqueda binaria en arbol

--- binaryTree.py
+++ binaryTree.py
@@ -1,50 +1,5 @@
 #!/usr/bin/env python
-
 class Node:
-    lft = None
-    rgt = None
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val   
-
-def insert(node, key, val):
-    
-    if node is None: return Node(key, val)
-    
-    if node.key == key: node.val = val
-    elif key < node.key:
-        
-         node.lft = insert(node.lft, key, val)   
-    else:
-        #A la derecha!
-         node.rgt = insert(node.rgt, key, val)   
-    return node
-            
-def search(node, key):
-    #Hoja Vacia: Si no esta presente
-    if node is None: raise KeyError
-    #Buscando Key: retorna valor
-    if node.key == key:
-        #Menor que key
-        return node.val                         
-    elif key < node.key:
-         return search(node.lft, key)
-    else:
-         return search(node.rgt, key)
-
-class Tree:
-    root = None
-    
-    def __setitem__(self, key, val):
-        self.root = insert(self.root, key, val)
-    def __getitem__(self, key): return search(self.root, key)
-    def __contains__(self, key):
-        try: search(self.root, key)
-        except KeyError: return False
-        return True
-    
-    
-    class Node:
     lft = None
     rgt = None
     def __init__(self, key, val):
